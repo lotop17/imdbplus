708dda7385193a1541f1f2d62257d0cf21225039
jundevriendt <Jun.Devriendt@Ugent.be>
2022-01-02 19:20:00 +0100

    save

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,29 @@
 import styles from "../styles/Footer.module.scss"
+import { FacebookShareButton, FacebookIcon } from "next-share"
+
 
 const Footer = () => {
   return (
     <footer className={styles.footerwrapper}>
+      <div className="sharebar">
+           Share onze website op facebook ---> <FacebookShareButton url={'https://imdbplus-teal.vercel.app/'}
+            quote={'Bring the festivals back to life/live'}
+            hastag={'#festivals'}><FacebookIcon size={40} round /></FacebookShareButton>
+          </div>
       <div className={styles.footer}>
 
+
         <div className="">
-          <img
+        <h>Website made by Emiel, Jun, Tony, Robbe, Jarne, Louis, Gilles and Rilke</h>
+          <p><img
             src="https://a.storyblok.com/f/138109/490x324/695fd5fe50/logofestival.JPG"
             alt="Festival Min Logo"
             className=""
-          />
+
+          /></p>
         </div>
-        <p>Website made by Emiel, Jun, Tony, Robbe, Jarne, Louis, Gilles and Rilke</p>
       </div>
+    
     </footer>
 
 
