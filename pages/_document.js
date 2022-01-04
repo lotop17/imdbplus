5bdfe222d95d825727f82aaa229ed914028058ed
jundevriendt <Jun.Devriendt@Ugent.be>
2022-01-04 23:22:10 +0100

    tawkto

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/61cdb0d680b2296cfdd46374/1fo5nli0p'';
+            s1.src='https://embed.tawk.to/61cdb0d680b2296cfdd46374/1fo5nli0p';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
